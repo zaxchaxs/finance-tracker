b0d689818f8c576a2886cacfd795fc3b08e75864
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-08-15 20:56:31 +0700

    .

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+    return <>
+    <main className="min-h-screen p-5 relative z-10 text-secondary bg-primary w-full flex flex-col items-center justify-center py-10 pb-14">
+        <h1 className="text-lg font-title">{`There's Nothing Here`}</h1>
+    </main>
+    </>
+};
+
+export default NotFoundPage;
