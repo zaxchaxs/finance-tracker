fc7c7b979a53f03d85aaa8b03939c7ebe9e7b7fe
zaxchaxs <zaxchaxsecondid@gmail.com>
2024-11-25 16:02:23 +0700

    Add Shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,84 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        "primary": "#E1FAEE",
-        "secondary": "#047857",
-        "secondary-hover": "#049669",
-        'third': '#CCFBF1',
-        'third-hover': '#F0FDFA',
-        "danger": "#FF1D48",
-        "danger-hover": "#F43F5E",
-      },
-      textColor: {
-        "primary": "#052E16",
-        // "secondary": "#15803D",
-        "secondary": "#2E2E2E",
-        "lightGreen": "#BBF7D0",
-        "lightWhite": "#F1F5F9"
-      },
-      fontFamily: {
-        'header': ['var(--font-blinker)'],
-        'title': ['var(--font-encodeSans)'],
-        'paragraf': ['var(--font-roboto)']
-      },
-      borderRadius: {
-        '5xl': '3rem'
-      }
-    },
+  	extend: {
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
+  		colors: {
+  			'primary': '#E1FAEE',
+  			'secondary': '#047857',
+  			'secondary-hover': '#049669',
+  			third: '#CCFBF1',
+  			'third-hover': '#F0FDFA',
+  			'danger': '#FF1D48',
+  			'danger-hover': '#F43F5E',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		textColor: {
+  			'primary': '#052E16',
+  			'secondary': '#2E2E2E',
+  			'lightGreen': '#BBF7D0',
+  			'lightWhite': '#F1F5F9'
+  		},
+  		fontFamily: {
+  			header: ['var(--font-blinker)'],
+  			title: ['var(--font-encodeSans)'],
+  			paragraf: ['var(--font-roboto)']
+  		},
+  		borderRadius: {
+  			'5xl': '3rem',
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
